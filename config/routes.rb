1e86bd5e33b6794ff7c376f90574f74d466dfbbd
Steve McGee <steve.mcgee@instructure.com>
2023-03-16 10:43:31 -0400

    gravatar done

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,13 @@ Rails.application.routes.draw do
   get "/about", to: "static_pages#about"
   get "/contact", to: "static_pages#contact"
   get "/signup", to: "users#new"
+  resources :users
 end
 
 # using the GET paths above (ie. /about) automagically gives us both
 # about_path - which would be `/about`
 # about_url - which would be `http://www.example.com/about`
+
+# using 'resources' above gives us RESTful actions from the Users controller
+# first notably is the show action. ie. `/users/1` will give us the
+# show action on user 1
